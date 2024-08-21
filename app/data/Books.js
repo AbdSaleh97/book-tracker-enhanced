aa99c390064a4a14ddfb2668d423daa6893248f2
AbdSaleh97 <salehabd.897@gmail.com>
2024-08-21 19:15:22 +0300

    rlreal-time adding of books

---
+++ app/data/Books.js
@@ -0,0 +1,42 @@
+export const books = [
+    {
+      id: 1,
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      genre: "Fiction",
+      publishedDate: "1960-07-11",
+      isbn: "978-0-06-112008-4",
+      price: "$10.99",
+      description: "A novel about the serious issues of rape and racial inequality, narrated by young Scout Finch in the Depression-era South."
+    },
+    {
+      id: 2,
+      title: "1984",
+      author: "George Orwell",
+      genre: "Dystopian",
+      publishedDate: "1949-06-08",
+      isbn: "978-0-452-28423-4",
+      price: "$8.99",
+      description: "A dystopian novel set in a totalitarian society ruled by Big Brother, exploring themes of surveillance, censorship, and individuality."
+    },
+    {
+      id: 3,
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      genre: "Classics",
+      publishedDate: "1925-04-10",
+      isbn: "978-0-7432-7356-5",
+      price: "$12.99",
+      description: "A tale of the Jazz Age and the American Dream, following the mysterious Jay Gatsby and his love for Daisy Buchanan."
+    },
+    {
+      id: 4,
+      title: "Harry Potter and the Sorcerer's Stone",
+      author: "J.K. Rowling",
+      genre: "Fantasy",
+      publishedDate: "1997-06-26",
+      isbn: "978-0-590-35340-3",
+      price: "$9.99",
+      description: "The first book in the Harry Potter series, introducing the young wizard Harry Potter and his adventures at Hogwarts School of Witchcraft and Wizardry."
+    }  
+];
